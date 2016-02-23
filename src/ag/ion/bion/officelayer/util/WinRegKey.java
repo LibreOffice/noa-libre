13f09f4b11221f4aabfd379cae046e9efedcc3ae
Katarina Behrens <Katarina.Behrens@cib.de>
2016-02-23 22:58:09 +0100

    LGPL license header for the new files

--- src/ag/ion/bion/officelayer/util/WinRegKey.java
+++ src/ag/ion/bion/officelayer/util/WinRegKey.java
@@ -1,9 +1,25 @@
 /*
- * This file is part of the LibreOffice project.
+ * This file is part of noa-libre.
  *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * The Contents of this file are made available subject to
+ * the terms of GNU Lesser General Public License Version 2.1.
+ *
+ * GNU Lesser General Public License Version 2.1
+ * ========================================================================
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Publi
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston
+ * MA  02111-1307  USA
  *
  * This file incorporates work covered by the following license notice:
  *
